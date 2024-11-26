866bffd6d283fc2607632eb11987e57797c3784e
Manbirs04 <144939777+Manbirs04@users.noreply.github.com>
2024-11-25 17:21:19 -0700

    Delete violated/SRP directory

--- violated/SRP/ReportGenerator.java
+++
@@ -1,9 +0,0 @@
-public class ReportGenerator {
-    public void generateReport() {
-        System.out.println("Generating report...");
-    }
-
-    public void saveToFile(String filePath) {
-        System.out.println("Saving report to file...");
-    }
-}

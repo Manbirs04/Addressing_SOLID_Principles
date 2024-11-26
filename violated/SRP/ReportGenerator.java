b561f930ecd2efc0123d033f195f05cad25e7698
Manbirs04 <144939777+Manbirs04@users.noreply.github.com>
2024-11-25 17:19:21 -0700

    Create ReportGenerator.java

---
+++ violated/SRP/ReportGenerator.java
@@ -0,0 +1,9 @@
+public class ReportGenerator {
+    public void generateReport() {
+        System.out.println("Generating report...");
+    }
+
+    public void saveToFile(String filePath) {
+        System.out.println("Saving report to file...");
+    }
+}
